0c7ffbbfa7e7808206e59c7f983d5e84ffd3631b
Curtis <curtis@cstudio.ca>
2022-08-27 16:32:48 -0700

    additions

---
+++ config/allowedOrigins.js
@@ -0,0 +1,8 @@
+const allowedOrigins = [
+	'http://10.0.0.84:3000',
+	'http://localhost:3000',
+	'https://www.macro.cstudio.ca',
+	'https://macro.cstudio.ca',
+];
+
+module.exports = allowedOrigins;
--- server.js
+++ server.js
@@ -1,32 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const corsOptions = require('./config/corsOptions');
 const connectDB = require('./config/dbCon');
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3500;
 
-// CORS ---
-const allowedOrigins = [
-	'https://macro.cstudio.ca',
-	'http://10.0.0.84:3000',
-	'http://localhost:3000',
-];
-const corsOptions = {
-	origin: (origin, callback) => {
-		if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-			callback(null, true);
-		} else {
-			callback(new Error('Not allowed by CORS'));
-		}
-	},
-	credentials: true,
-	optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-
-// CONNECT TO MONGODB ---
+// MONGODB ---
 connectDB();
+
+// CORS ---
+app.use(cors(corsOptions));
 
 // FOR URLENCODED FORM DATA
 app.use(express.urlencoded({ extended: false }));
@@ -34,9 +21,24 @@ app.use(express.urlencoded({ extended: false }));
 // FOR JSON ---
 app.use(express.json());
 
+// COOKIE PARSER ---
+app.use(cookieParser());
+
 // ROUTES ---
+app.use('/', express.static(path.join(__dirname, 'public')));
 app.use('/food', require('./routes/food'));
 app.use('/log', require('./routes/log'));
+
+app.all('*', (req, res) => {
+	res.status(404);
+	if (req.accepts('html')) {
+		res.sendFile(path.join(__dirname, 'views', '404.html'));
+	} else if (req.accepts('json')) {
+		res.json({ message: '404 Not Found' });
+	} else {
+		res.type('txt').send('404 Not Found');
+	}
+});
 
 // LISTEN ---
 mongoose.connection.once('open', () => {
